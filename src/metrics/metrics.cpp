535750902002cf60ca2ad6c6cf2f0d072881f72d
Michael Piggott <michaeltpiggott@gmail.com>
2011-07-13 21:50:32 -0700

    Restarting with python

--- src/metrics/metrics.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Author:      Michael Piggott
- * Date:        06/24/2011
- * File:        metrics.cpp
- * Description: Metrics used to determine quality of roads
- */
-
-/* Includes */
-#include "metrics.h"
